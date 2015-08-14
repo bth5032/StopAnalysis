9a8f9913396fafd917f74f42933cdc76c913bd4d
Bobak <bth5032@gmail.com>
2015-08-14 13:38:34 -0700

    testing code

--- StopBabyMaker/batch/batch.sh
+++ StopBabyMaker/batch/batch.sh
@@ -16,8 +16,11 @@ verbose="false"
 #Set directory to copy results to when finished
 copy_dir="$HOME/test/"
 if [ ! -d $copy_dir/merged_files ]; then
+    echo "1"
     mkdir $copy_dir/merged_files
+    echo "2"
 fi
+echo "3"
 
 #Check that log file folder and tarball files exists
 if [ ! -d logs ]; then echo "Run . copy.sh first!"; return 1; fi
@@ -63,11 +66,14 @@ do
   local_sample_dir=`cat ../sample.dat | grep -A2 $sample | grep "LocalPath" | cut -d ' ' -f2`
   hadoop_sample_dir=`cat ../sample.dat | grep -A2 $sample | grep "HadoopPath" | cut -d ' ' -f2`
 
+echo "for loop"
+
   for file in `/bin/ls $hadoop_sample_dir/merged_ntuple_*.root`; do
-
+echo "5"
     number_temp=${file##*/merged_ntuple_}
+echo "6"
     number=${number_temp%*.root}
-
+echo "7"
     #Except they've finished
     if [ -e $copy_dir/${sample}_${number}.root ] 
     then 
