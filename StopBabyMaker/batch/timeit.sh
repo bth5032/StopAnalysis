f2ce4b8dc027c7aa8c3c46abcbf430c1725faeb0
Bobak <bth5032@gmail.com>
2015-08-18 15:04:20 -0700

    added script to time the running of batch.sh, and also moved over condor files that make the test runable by Bobak

---
+++ StopBabyMaker/batch/timeit.sh
@@ -0,0 +1,14 @@
+# This script calls batch.sh and then keeps checking condor_q for files
+# until there are no more files submitted by the user.
+
+echo "Unix time at start: " `date +%s`
+./batch.sh
+
+while 1
+do
+	if [[ $(condor_q | grep `whoami`) == "" ]]
+		break
+done 
+
+echo "Time at finish: " `date +%s`
+
