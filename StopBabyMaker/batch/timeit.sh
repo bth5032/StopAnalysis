3be870d44c761098fbe7502b5e648b2ec2c81017
bth5032@gmail.com <bhashemi@uaf-4.t2.ucsd.edu>
2015-08-18 16:56:13 -0700

    Replaced timeit with the proper one from IOTest

--- StopBabyMaker/batch/timeit.sh
+++ StopBabyMaker/batch/timeit.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
+
 # This script calls batch.sh and then keeps checking condor_q for files
 # until there are no more files submitted by the user.
 
 echo "Unix time at start: " `date +%s`
 . batch.sh
 
-while 1
+while [ 1 ]
 do
 	if [[ $(condor_q | grep `whoami`) == "" ]]
+	then
 		break
+	fi
+	sleep 10
 done 
 
 echo "Time at finish: " `date +%s`
